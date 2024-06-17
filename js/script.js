9211b3158633f4f55d419e1fe475026907b165d2
Liander Souza <liandervinicius13@gmail.com>
2024-06-16 22:01:50 -0300

    section contato

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 //Menu
 const buttonMenu = document.querySelector('.menu-mobile');
 const menu = document.querySelector('header .container nav ul');
+const buttonEnviarMensagem = document.querySelector('.enviar-mensagem');
 
 buttonMenu.addEventListener('click', () => {
     menu.classList.toggle('menu-mobile-active');
 
+    const lines = document.querySelectorAll('header .container .button-menu-mobile .line');
+    
+    lines.forEach(line => line.classList.toggle('menu-active'));
     const links = document.querySelector('.links')
     links.addEventListener('click', () => {
         menu.classList.remove('menu-mobile-active');
